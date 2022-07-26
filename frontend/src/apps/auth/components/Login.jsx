3a9c7bf889aa34340b97825b84fdd3958d8ca32d
theakhilleus <black_the_dragon@hotmail.com>
2022-07-26 16:35:42 +0300

    updated css

--- frontend/src/apps/auth/components/Login.jsx
+++ frontend/src/apps/auth/components/Login.jsx
@@ -1,78 +1,87 @@
 import React, { useContext, useState } from "react";
 import FirebaseContext from "../../../context/FirebaseContext";
+import { useNavigate } from "react-router-dom";
+import "./styles.css";
 const Login = () => {
   const { login } = useContext(FirebaseContext);
   const { signGoogle } = useContext(FirebaseContext);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-
+  const nav = useNavigate();
   return (
-    <div
-      style={{
-        width: "50%",
-        marginLeft: "auto",
-        marginRight: "auto",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <h1>Please Login to Continue</h1>
-      <div
-        className="social_login_buttons"
-        style={{ margin: "18%", alignContent: "space-evenly" }}
-      >
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            signGoogle();
-          }}
-          style={{ marginLeft: "10%", width: "3%" }}
-        >
-          <i className="fa-brands fa-google"></i>
-        </button>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            signGoogle();
-          }}
-          style={{ marginLeft: "10%", width: "3%" }}
-        >
-          <i className="fa-brands fa-facebook"></i>
-        </button>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            signGoogle();
-          }}
-          style={{ marginLeft: "10%", width: "3%" }}
-        >
-          <i className="fa-brands fa-github"></i>
-        </button>
+    <div className="big_container">
+      <div className="left_bar">
+        <div className="left_bar_children">
+          <h1>Don't You Have An Account Yet ?</h1>
+          <label htmlFor="reg_button">It's So Easy To Go On</label>
+          <button onClick={
+            ()=>nav("../register", { replace: true })
+            } 
+          id="reg_button">
+            Click To Sign Up
+          </button>
+        </div>
       </div>
-      <form>
-        <input
-          type="email"
-          placeholder="email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          style={{ display: "block", marginLeft: "auto", marginRight: "auto" }}
-          onClick={(e) => {
-            e.preventDefault();
-            login(email, password);
-          }}
-        >
-          Register
-        </button>
-      </form>
+
+      <div className="loginContainer">
+        <h1>Please Login to Continue...</h1>
+        <div className="social_login_buttons">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              signGoogle();
+            }}
+            style={{ color: "#de5246" }}
+          >
+            <i className="fa-brands fa-google"></i>
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              signGoogle();
+            }}
+            style={{ color: "#4267B2" }}
+          >
+            <i className="fa-brands fa-facebook"></i>
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              signGoogle();
+            }}
+            style={{ color: "grey" }}
+          >
+            <i className="fa-brands fa-github"></i>
+          </button>
+        </div>
+        <form className="form_login">
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            style={{
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+            onClick={(e) => {
+              e.preventDefault();
+              login(email, password);
+            }}
+          >
+            Log me In
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/apps/auth/components/Register.jsx
+++ frontend/src/apps/auth/components/Register.jsx
@@ -1,29 +1,40 @@
-import React,{useContext,useState} from 'react'
-import FirebaseContext from '../../../context/FirebaseContext';
+import React, { useContext, useState } from "react";
+import FirebaseContext from "../../../context/FirebaseContext";
+import "./reg_styles.css";
 const Register = () => {
-  const {register} = useContext(FirebaseContext);
-  const [email,setEmail] = useState();
-  const [password,setPassword] = useState();
+  const { register } = useContext(FirebaseContext);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   return (
-  <form >
-    <input type="email" 
-    placeholder='email' 
-    onChange={
-      (e)=>{setEmail(e.target.value)}} 
-
-      />
-    <input type="password"
-    placeholder='password' 
-    onChange={
-      (e)=>setPassword(e.target.value)} 
-
-      />
-    <button onClick={(e)=>{
-      e.preventDefault();
-      register(email,password)
-    }}>Register</button>
-  </form>
-  )
-}
+    <div className="reg_container">
+    <div className="reg_container_children">
+    <h2>Welcome To New World Of Chatting!</h2>
+    <h3>Sign Up To Join Us</h3>
+      <form className="form_reg">
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            register(email, password);
+          }}
+        >
+          Register
+        </button>
+      </form>
+    </div>
+    </div>
+  );
+};
 
 export default Register;
